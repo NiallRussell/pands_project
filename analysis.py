09bde38de0e9b45f418dfef098f0261e37c3e824
Niall.Russell <niall.russell@ul.ie>
2024-05-10 11:02:05 +0100

    start histogram

--- analysis.py
+++ analysis.py
@@ -1,6 +1,12 @@
 #Import pandas for data manipulation using dataframes
 import pandas as pd
 
+#Import matplotlib.pyplot to generate plots
+import matplotlib.pyplot as plt
+
+#Import numpy to create arrays from pandas dataframe
+import numpy as np
+
 #Read in dataset
 df = pd.read_csv("iris.csv")
 
@@ -15,3 +21,19 @@ with open ("iris_summary.txt", "a") as f:
            f.write(descriptives.to_string())
            f.write("\n")
            f.write(species_count.to_string())
+
+
+sepal_length = df["sepal_length"].to_numpy()
+sepal_width = df["sepal_width"].to_numpy()
+petal_length = df["petal_length"].to_numpy()
+petal_width = df["petal_width"].to_numpy()
+
+plt.hist(sepal_length)
+plt.savefig('sepal_length.png')
+plt.hist(sepal_width)
+plt.savefig('sepal_width.png')
+plt.hist(petal_length)
+plt.savefig('petal_length.png')
+plt.hist(petal_width)
+plt.savefig('petal_width.png')
+#https://stackoverflow.com/questions/9622163/save-plot-to-image-file-instead-of-displaying-it
